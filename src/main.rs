7008932d2ac867f430607bac068652f8e9b323be
Felipe <felipe@frontm.com>
2023-11-28 17:36:02 -0300

    feat: stage1

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
 use std::fs;
 
-fn main() {
+use anyhow::{Ok, Result};
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+#[clap(rename_all = "snake_case")]
+enum Command {
+    ///init
+    Init,
+}
+
+fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
+    let args = Args::parse();
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+    match args.command {
+        Command::Init => {
+            fs::create_dir(".git")?;
+            fs::create_dir(".git/objects")?;
+            fs::create_dir(".git/refs")?;
+            fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
+            println!("Initialized git directory")
+        }
+    }
+    return Ok(());
 }
